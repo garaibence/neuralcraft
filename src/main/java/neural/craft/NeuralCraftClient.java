020f100b3c8b5c365271940ebfd814a37da77518
garaibence <garaibence10@gmail.com>
2025-01-06 02:19:12 +0100

    initial commit

---
+++ src/main/java/neural/craft/NeuralCraftClient.java
@@ -0,0 +1,10 @@
+package neural.craft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NeuralCraftClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        // This code runs after the client has been initialized.
+    }
+}
